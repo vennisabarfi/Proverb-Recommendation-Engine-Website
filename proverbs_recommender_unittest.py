d15d6fe108d3b231fc8bdc8df1a247c8590048ac
Vennisa Owusu-Barfi <vennisabarfi@gmail.com>
2024-03-06 22:45:46 -0600

    Proverbs Recommender Unit Test
    Add unit tests for recommender script

---
+++ proverbs_recommender_unittest.py
@@ -0,0 +1,50 @@
+import unittest
+import os
+
+# Import functions to be tested
+from your_script_name import load_data_and_model, compute_proverb_embeddings, save_embeddings, load_embeddings, compute_proverbs_similarity, compute_meanings_similarity
+
+class TestProverbFunctions(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Set up necessary variables and data before running tests
+        cls.tokenizer, cls.model, cls.normalized_meanings, cls.normalized_proverbs = load_data_and_model()
+
+        # Compute embeddings for proverbs
+        cls.proverb_embeddings = compute_proverb_embeddings(cls.tokenizer, cls.model, cls.normalized_meanings)
+
+        # Save embeddings to file
+        save_embeddings(cls.proverb_embeddings, "test_proverb_embeddings.npy")
+
+        # Load embeddings from file
+        cls.loaded_embeddings = load_embeddings("test_proverb_embeddings.npy")
+
+    @classmethod
+    def tearDownClass(cls):
+        # Clean up after running tests
+        os.remove("test_proverb_embeddings.npy")
+
+    def test_compute_proverb_embeddings(self):
+        # Test if the function returns embeddings of correct shape
+        self.assertEqual(self.proverb_embeddings.shape[0], len(self.normalized_meanings))
+        self.assertEqual(self.proverb_embeddings.shape[1], 768)  # Assuming DistilBERT embeddings
+
+    def test_save_and_load_embeddings(self):
+        # Test if the saved embeddings can be successfully loaded and have the same content
+        loaded_embeddings = load_embeddings("test_proverb_embeddings.npy")
+        self.assertTrue((self.proverb_embeddings == loaded_embeddings).all())
+
+        def test_compute_proverbs_similarity(self):
+        # Test if the function returns a list of similar proverbs
+        similar_proverbs = compute_proverbs_similarity("test input", self.tokenizer, self.model, self.loaded_embeddings, self.proverbs, self.meanings)
+        self.assertIsInstance(similar_proverbs, list)
+        self.assertGreater(len(similar_proverbs), 0)
+
+    def test_compute_meanings_similarity(self):
+        # Test if the function returns a list of similar meanings
+        similar_meanings = compute_meanings_similarity("test input", self.tokenizer, self.model, self.loaded_embeddings, self.proverbs, self.meanings)
+        self.assertIsInstance(similar_meanings, list)
+        self.assertGreater(len(similar_meanings), 0)
+
+if __name__ == '__main__':
+    unittest.main()
